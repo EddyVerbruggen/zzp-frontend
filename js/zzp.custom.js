29c6be553be72daa733067583479546ba96fd260
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-05 23:34:10 +0200

    Added generic errorhandling

--- js/zzp.custom.js
+++ js/zzp.custom.js
@@ -1,3 +1,9 @@
+// generic catch for errors
+window.onerror = function(message, file, line) {
+  alert('Error gevangen: ' + file + ':' + line + '\n' + message);
+};
+
+// override some JQM defaults
 $(document).bind("mobileinit", function(){
   $.extend(
       $.mobile, {
