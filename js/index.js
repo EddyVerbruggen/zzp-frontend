16572913555403c2d532644f8de9ffc55e1419b8
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-07 11:20:34 +0200

    - added jobupdate page - map indicators are now numbers instead of dots (micro infographics, baby ;)) - split css / js per page - added more dummydate to explain the app

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 function makeStatusEditable() {
-  $('#status').attr('contenteditable', true);
-  $('#status').css({'border': '1px dotted #666'});
-  $('#editButton').hide();
-  $('#saveButton').show();
+  $('#indexHeader #status').attr('contenteditable', true);
+  $('#indexHeader #status').css({'border': '1px dotted #666'});
+  $('#indexHeader #editButton').hide();
+  $('#indexHeader #saveButton').show();
 }
 
 function saveStatus() {
-  $('#status').attr('contenteditable', false);
-  $('#status').css({'border': '0'});
-  $('#saveButton').hide();
-  $('#editButton').show();
-  $('#statusDate').append(' (TODO: refresh)');
+  $('#indexHeader #status').attr('contenteditable', false);
+  $('#indexHeader #status').css({'border': '0'});
+  $('#indexHeader #saveButton').hide();
+  $('#indexHeader #editButton').show();
+  $('#indexHeader #statusDate').append(' (TODO: refresh)');
 }
---
+++ js/userupdate.js
@@ -0,0 +1,18 @@
+function makeCommentEditable() {
+  $('#suggestComment').css('opacity', 1);
+  $('#suggestComment #comment').attr('contenteditable', true);
+  $('#suggestComment #comment').css({'border': '1px dotted #666'});
+  $('#suggestComment #editButton').hide();
+  $('#suggestComment #saveButton').show();
+}
+
+function saveComment() {
+  if ($('#suggestComment #comment').html() == "schrijf een reactie...") {
+    $('#suggestComment').css('opacity', 0.4);
+  }
+  $('#suggestComment #comment').attr('contenteditable', false);
+  $('#suggestComment #comment').css({'border': '0'});
+  $('#suggestComment #saveButton').hide();
+  $('#suggestComment #editButton').show();
+//  $('#suggestComment #commentDate').append(' (TODO: refresh)');
+}
