abd0e51afdd7f3c47544d2f2f59284c5e3c53f68
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-07 14:07:11 +0200

    Added pull to refresh :)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
+$(document).ready(function() {
+  $('#networkUpdatesScroller, #jobUpdatesScroller').scrollz({
+    pull: true
+  });
+
+  $('#networkUpdatesScroller').bind('pulled', function () {
+    // Process pull action here
+    alert('fetching network updates..');
+
+    // Hide pull header when done
+    $('#networkUpdatesScroller').scrollz('hidePullHeader');
+  });
+
+  $('#jobUpdatesScroller').bind('pulled', function () {
+    // Process pull action here
+    alert('fetching job updates..');
+
+    // Hide pull header when done
+    $('#jobUpdatesScroller').scrollz('hidePullHeader');
+  });
+});
+
 function makeStatusEditable() {
   $('#indexHeader #status').attr('contenteditable', true);
-  $('#indexHeader #status').css({'border': '1px dotted #666'});
+  $('#indexHeader #status').css({'border':'1px dotted #666'});
   $('#indexHeader #editButton').hide();
   $('#indexHeader #saveButton').show();
 }
 
 function saveStatus() {
   $('#indexHeader #status').attr('contenteditable', false);
-  $('#indexHeader #status').css({'border': '0'});
+  $('#indexHeader #status').css({'border':'0'});
   $('#indexHeader #saveButton').hide();
   $('#indexHeader #editButton').show();
   $('#indexHeader #statusDate').append(' (TODO: refresh)');
