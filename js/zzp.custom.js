d34075c474e61b09f60f1842d627d1428e095ebc
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-08 23:09:24 +0200

    LinkedIn PoC - checkin for phonegap test

--- js/zzp.custom.js
+++ js/zzp.custom.js
@@ -10,3 +10,11 @@ $(document).bind("mobileinit", function(){
         defaultPageTransition: "slide"
       })
 });
+
+function getUrlParam(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search)) {
+     return decodeURIComponent(name[1]);
+   } else {
+     return null;
+   }
+}
