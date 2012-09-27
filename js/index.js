d6601bfe4e473335063701dfd1a2ed442a0325be
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-27 23:50:59 +0200

    Implemented cewl networkupdate filter

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
+$(document).ready(function() {
+  $("#filterFavourites, #filterCompany, #filterLinkedIn").click(function() {
+    filterNetworkUpdates();
+  });
+});
+
+function filterNetworkUpdates() {
+  // check which filters are active
+  var favouritesActive = $("#filterFavourites").attr("checked") == "checked";
+  var companyActive = $("#filterCompany").attr("checked") == "checked";
+  var linkedInActive = $("#filterLinkedIn").attr("checked") == "checked";
+
+  // for each update..
+  $(".networkUpdates").each(function() {
+    // track a boolean for weather or not to show it (if none is active, show is true)
+    var show = !favouritesActive && !companyActive && !linkedInActive;
+    // inspect all icons
+    $(this).find("img.nutIcon").each(function() {
+      // if one of the active filters is a match, show the icon
+      var networkUpdateType = $(this).attr('data-name');
+      if ((favouritesActive && networkUpdateType == "favourite") ||
+          (companyActive && networkUpdateType == "company") ||
+          (linkedInActive && networkUpdateType == "linkedin")) {
+        show = true;
+      }
+    });
+    // deal with the calculated show value
+    if (show) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+}
+
 function makeStatusEditable() {
   $('#indexHeader #status').attr('contenteditable', true);
   $('#indexHeader #status').css({'border':'1px dotted #666'});
