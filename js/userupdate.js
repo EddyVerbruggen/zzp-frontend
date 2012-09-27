479ea5124d50833d01ba8488a11dcdd2f4c75aac
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-27 23:04:15 +0200

    Fixes, layout tweaks, cleanup

--- js/userupdate.js
+++ js/userupdate.js
@@ -1,12 +1,17 @@
 function makeCommentEditable() {
   $('#suggestComment').css('opacity', 1);
   $('#suggestComment #comment').attr('contenteditable', true);
+  $('#suggestComment #comment').html("");
+  $('#suggestComment #comment').focus();
   $('#suggestComment #comment').css({'border': '1px dotted #666'});
   $('#suggestComment #editButton').hide();
   $('#suggestComment #saveButton').show();
 }
 
 function saveComment() {
+  if ($('#suggestComment #comment').html().trim() == "") {
+    $('#suggestComment #comment').html("schrijf een reactie...");
+  }
   if ($('#suggestComment #comment').html() == "schrijf een reactie...") {
     $('#suggestComment').css('opacity', 0.4);
   }
